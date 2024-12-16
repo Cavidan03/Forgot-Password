41d28008391f695b3e34fff1fdaefe8213bca18e
Cavidan03 <aa4060918@gmail.com>
2024-12-16 13:12:34 +0400

    Changes the Port

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db= new PDO("mysql:host=localhost;dbname=form;charset-utf8","root", "");
+    $db= new PDO("mysql:host=localhost;port=4306;dbname=form;charset-utf8","root", "");
 } 
 catch (PDOException $e) {
     echo $e->getMessage();
